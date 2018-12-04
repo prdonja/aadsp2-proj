fcc68481e4dfbece5a5c6dd456fe8ca03c7d8f31
student <student@domain.local>
2018-12-04 15:02:38 +0100

    model 1 done, working on model 2

--- Projekat/model1/ProcessWavFile/define.h
+++ Projekat/model1/ProcessWavFile/define.h
@@ -2,3 +2,7 @@
 #define MAX_NUM_CHANNEL 8
 
 #define DEFAULT_GAIN 0.5
+
+typedef short DSPshort;
+typedef int DSPint;
+typedef unsigned short DSPushort;
---
+++ Projekat/model2/ProcessWavFile/define.h
@@ -0,0 +1,10 @@
+#define BLOCK_SIZE 16
+#define MAX_NUM_CHANNEL 8
+
+#define DEFAULT_GAIN 0.5
+
+typedef short DSPshort;
+typedef int DSPint;
+typedef unsigned short DSPushort;
+typedef double DSPfract;
+typedef double DSacum;
